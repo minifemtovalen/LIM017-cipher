50ff44bba8c601c2f9dde863a5452c3b22467f94
Osmar Reyes <osmar@lc.dev>
2022-02-08 14:15:24 -0500

    add cipher encode && decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-  // ...
-};
+encode: (offset, msg) => {
+  const charArr = msg.toUpperCase();
+  let cipherWord = '';
+  for (let i = 0; i < charArr.length; i++) {
+    let character = charArr[i];
+    let asciiPos = character.charCodeAt(0);
+    let offsetFormula = ((asciiPos - 65 + parseInt(offset)) % 26) + 65;
+    if (asciiPos >= 65 && asciiPos <= 90) {
+      cipherWord += String.fromCharCode(offsetFormula);
+    } else {
+      cipherWord += character;
+    }
+  }
+  return cipherWord;
+},
+decode: (offset, msg) => {
+  const charArr = msg.toUpperCase();
+  let decodedWord = '';
+  for (let i = 0; i < charArr.length; i++) {
+    let character = charArr[i];
+    let asciiPos = character.charCodeAt(0);
+    let offsetFormula = ((asciiPos + 65 - parseInt(offset)) % 26) + 65;
+    if (asciiPos >= 65 && asciiPos <= 90) {
+      decodedWord += String.fromCharCode(offsetFormula);
+    } else {
+      decodedWord += character;
+    }
+  }
+  return decodedWord;
+}
+
+}
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import cipher from './cipher.js';
-
 console.log(cipher);
+console.log(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+console.log(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"));
