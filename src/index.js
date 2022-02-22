c2c1bbcac6ada64135f6c3d6e43ddaf0e401f2ae
Valentina Patti <valentinapatti23@gmail.com>
2022-02-22 01:23:00 -0500

    add reset btn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import cipher from './cipher.js';
-console.log(cipher);
-console.log(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-console.log(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"));
+const inputMsg = document.querySelector('#text-msg');
+const offsetInput = document.querySelector('#offset');
+const outputMsg = document.querySelector('#final-msg');
+const switchBtn = document.querySelector('#switch-btn');
+let btnStatus = true;
+const resetBtn = document.querySelector('#reset-btn');
+
+switchBtn.addEventListener('click', () => {
+  btnStatus = !btnStatus;
+});
+
+inputMsg.oninput = () => {
+  let result = '';
+  if (btnStatus) {
+    result = cipher.encode(offsetInput.value, inputMsg.value)
+  } else {
+    result = cipher.decode(offsetInput.value, inputMsg.value)
+  }
+  outputMsg.value = result;
+}
+
+resetBtn.addEventListener('click', () => {
+  inputMsg.value = '';
+  outputMsg.value = '';
+});
+
